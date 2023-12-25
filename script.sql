aeafe5bcf2a70359ffafca7fc0eb3f47d300e809
MN911718 <128761614+MN911718@users.noreply.github.com>
2023-12-25 22:59:20 +0700

    Add files via upload

---
+++ script.sql
@@ -0,0 +1,5 @@
+create table users (user_id INT NOT NULL PRIMARY KEY, name TEXT, class TEXT);
+insert into users (user_id, name, class) values (20521518, 'Nguyen Thi Hong Lam', 'NT132.O11.ANTT');
+insert into users (user_id, name, class) values (20520823, 'Mai Ngoc Phuong Trinh', 'NT132.O11.ANTT');
+insert into users (user_id, name, class) values (21521195, 'Tran Le Minh Ngoc', 'NT132.O11.ANTT');
+insert into users (user_id, name, class) values (21522240, 'Huynh Minh Khue', 'NT132.O11.ANTT');
